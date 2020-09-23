36a1c21eb4b68514fd822d45f00f2124522f1f9b
Zetta Zone <zettazone24@gmail.com>
2020-09-23 13:27:38 +0300

    Third Template - Responsive Design - Themes's Files

---
+++ js/custom.js
@@ -0,0 +1,32 @@
+/* Custome */
+
+$(function () {
+    'use strict';
+    $('.info-list li').click(function(){
+        $(this).addClass('selected').siblings('li').removeClass('selected');
+        $('.info-content div').hide();
+        $('.'+$(this).data('class')).fadeIn();
+    });
+
+   /* $('.nav #menu-acces').click(function(){
+        $('.nav .nav-list').hide();
+        $('.'+$(this).data('class')).slideToggle();
+        /*$('.nav .nav-list').show();
+    });*/
+
+    $(".nav #menu-acces").click(function(){
+         $(".nav ul").removeClass("hidden-sm");
+        $(".nav .nav-list").toggle();
+    });
+});
+
+
+// Loading Screen
+$(document).ready(function() {
+    // document is loaded and DOM is ready
+    $(".loading-overlay").fadeOut(2000, function(){
+         $("body").css("overflow","auto");
+         $(this).remove();
+    });
+   
+});
