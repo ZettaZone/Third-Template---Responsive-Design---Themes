606315f8539e66d4de10ea50c342fa2c9d91727d
Zetta Zone <zettazone24@gmail.com>
2020-09-23 13:48:55 +0300

    modified:   js/custom.js

--- js/custom.js
+++ js/custom.js
@@ -21,12 +21,13 @@ $(function () {
 });
 
 
+
 // Loading Screen
-$(document).ready(function() {
-    // document is loaded and DOM is ready
-    $(".loading-overlay").fadeOut(2000, function(){
-         $("body").css("overflow","auto");
-         $(this).remove();
-    });
-   
+$(window).on('load', function(){ 
+   // document is loaded and DOM is ready
+   $(".loading-overlay").fadeOut(2000, function(){
+    $("body").css("overflow","auto");
+    $(this).remove();
 });
+});
+
